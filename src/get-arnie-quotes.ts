0024f2cdfb8a65bf355bd4138e4e4e40ce4ab1b1
Andres Amado <andres_amado@icloud.com>
2022-11-03 21:22:32 +1000

    implements getArnieQuotes function

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,19 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
-// TODO define this type properly
-type TResult = any;
+enum Statuses {
+  SUCCESS = "Arnie Quote",
+  FAILURE = "FAILURE"
+}
+
+type TResult = {[Statuses.SUCCESS] : string} | {[Statuses.FAILURE] : string};
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+  const promises = urls.map(url => httpGet(url));
+  return Promise.all(promises).then(responses => 
+    responses.map(response => 
+      response.status === 200
+      ? { [Statuses.SUCCESS]: JSON.parse(response.body).message}
+      : { [Statuses.FAILURE]: JSON.parse(response.body).message}
+    )
+  );
 };
